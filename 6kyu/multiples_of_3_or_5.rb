9258575324226e940880441103cca482b04628ad
Seiya Kawamoto <kid960805@gmail.com>
2022-05-01 00:08:47 +0900

    first commit

---
+++ 6kyu/multiples_of_3_or_5.rb
@@ -0,0 +1,17 @@
+# My Answer
+def solution(number)
+  sum = 0
+  _number = number
+  for i in 1..number
+    _number -= 1
+    if _number % 3 == 0 || _number % 5 == 0
+      sum += _number
+    end
+  end
+  sum
+end
+
+# Best Practice
+def solution(number)
+  (1...number).select {|i| i%3==0 || i%5==0}.inject(:+)
+end
