81deae2455ec56eb141fd4bea55cf9e551b6dbca
Seiya Kawamoto <kid960805@gmail.com>
2022-05-05 22:16:55 +0900

    add: create phone number

---
+++ 6kyu/create_phone_number.rb
@@ -0,0 +1,12 @@
+# My Answer
+def create_phone_number(numbers)
+  first = numbers[0..2].join
+  second = numbers[3..5].join
+  third = numbers[6..9].join
+  "(#{first}) #{second}-#{third}"
+end
+
+# Best Practice
+def createPhoneNumber(array)
+  '(%d%d%d) %d%d%d-%d%d%d%d' % array
+end
