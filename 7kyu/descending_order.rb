a8f91c45f3341d7148f5cc9fdb1ad0b272175709
Seiya Kawamoto <kid960805@gmail.com>
2022-05-15 16:12:33 +0900

    add: Descending Orderad

--- 7kyu/descending_order.rb
+++ 7kyu/descending_order.rb
@@ -8,3 +8,19 @@ end
 def descending_order(n)
   n.to_s.chars.sort.reverse.join.to_i
 end
+
+# sort, <=>演算子
+# sortのアルゴリズムがよくわかっていない
+require 'date'
+
+data = [
+  {id: 1, name: '吉田(A)', ruby: 'yoshida', join_date: Date.new(2009, 4, 1)},
+  {id: 2, name: '鈴木', ruby: 'suzuki', join_date: Date.new(2015, 4, 1)},
+  {id: 3, name: '吉田(B)', ruby: 'yoshida', join_date: Date.new(2009, 4, 1)},
+  {id: 4, name: '佐藤', ruby: 'sato', join_date: Date.new(2006, 10, 1)},
+  {id: 5, name: '田中', ruby: 'tanaka', join_date: Date.new(2009, 4, 1)},
+]
+
+data.sort do |a, b|
+  b[:id] <=> a[:id]
+end
