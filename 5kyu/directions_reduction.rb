ba97e4be75878833643a7cd1ce8bb2c4a62bd55f
Seiya Kawamoto <kid960805@gmail.com>
2022-06-02 20:59:18 +0900

    add: Directions Reduction

---
+++ 5kyu/directions_reduction.rb
@@ -0,0 +1,15 @@
+# Best Practice
+OPPOSITE = {
+  "NORTH" => "SOUTH",
+  "SOUTH" => "NORTH",
+  "EAST"  => "WEST",
+  "WEST"  => "EAST"
+}
+
+def dirReduc(arr)
+  stack = []
+  arr.each do |dir|
+    OPPOSITE[dir] == stack.last ? stack.pop : stack.push(dir)
+  end
+  stack
+end
